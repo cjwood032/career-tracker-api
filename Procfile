71b3519d103436adaa18bf5047a2bbce5209153d
cjwood032 <cjwood032@gmail.com>
2019-01-05 19:11:15 -0500

    building linkage, updating career controller

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: cd career-tracker-client && npm start
+api: bundle exec rails s -p 3001
--- app/controllers/API/careers_controller.rb
+++ app/controllers/API/careers_controller.rb
@@ -1,2 +1,32 @@
-class CareersController < ApplicationController
+class API::CareersController < ApplicationController
+    before_action :set_career, only: [:show, :update, :destroy]
+    def create
+       career= Career.new(career_params)
+       if career.save
+        render json: career
+       else
+        render json: {message: career.errors}, status: 400
+       end 
+    end
+
+    def index
+        render json: Career.all, include: :descriptions
+    end
+    
+    def show
+    end
+    
+    def update
+    end
+    def destroy
+    end
+    private
+    def career_params
+        params.require(:career).permit(:body) #update later
+    end
+    
+    def set_career
+        @career = Career.find(params[:id])
+    end
+
 end
