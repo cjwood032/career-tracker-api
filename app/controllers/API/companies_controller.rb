54ee2adbec2ce66fd6d2ae0655658a82e78634e6
Christopher Wood <Cjwood032@gmail.com>
2019-04-16 02:22:55 -0400

    Update companies_controller.rb
    Added CRUD actions

--- app/controllers/API/companies_controller.rb
+++ app/controllers/API/companies_controller.rb
@@ -1,3 +1,54 @@
-class Api::CompaniessController < ApplicationController
+class Api::CompaniesController < ApplicationController
+      before_action :set_company, only: [:show, :update, :destroy]
+    def create
+
+       company= Company.new(company_params)
+       if company.save
+        render json: company
+       else
+        render json: {message: company.errors}, status: 400
+       end 
+    end
+
+    def index
+        @companies = Company.all
+        render json: @companies
+    end
+    
+    def show
+        render json: @company
+    end
+    
+    def update
+        if @company.update(company_params)
+            render json: @company
+        else
+            render json: {message: @company.errors}, status: 400
+        end
+    end
+    
+    def destroy
+        if @company.destroy
+            render status: 204
+        else
+            render json:{message: "There was an error deleting the company"}, status: 400
+        end
+
+    end
+    private
+    def company_params
+        params.permit(
+            :company_name,
+            :company_link,
+            :company_glassdoor,
+            :location,
+            :reviews
+            )
+        
+    end
+    
+    def set_company
+        @company = Company.find(params[:id])
+    end
 
 end
