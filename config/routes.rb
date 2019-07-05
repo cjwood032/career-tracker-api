196607adffbbcb4d128e681160b348ec8d6780cd
cjwood032 <cjwood032@gmail.com>
2019-07-05 02:52:43 -0400

    fix route issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     resources :careers
     resources :details
-<<<<<<< HEAD
     get '/API/import', to: "careers#import"
-=======
-    resources :companies do
-      resources :reviews
-    end
->>>>>>> 599329487e8d63ece48905b4bc3f71045742f84d
   end
 end
