d5d4737246f78a2014261ff48c1c9bc811460302
cjwood032 <cjwood032@gmail.com>
2019-07-05 16:39:38 -0400

    Import now saves all details

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,3 +1,46 @@
 class Detail < ApplicationRecord
-
+    def establish_session
+        session = GoogleDrive::Session.from_config("config.json")
+        spreadsheet_id = ENV["SHEET_ID"]
+        @sheets = session.spreadsheet_by_key(spreadsheet_id).worksheets
+    end
+    def get_activity
+        self.establish_session
+        active=@sheets[2].rows
+        active = active.drop(1)
+        active.each {|row| create_detail(row)}
+        return active
+    end
+    def create_detail(a)
+        if a[0] == "" || a[0] == " " || a[0] == nil  
+            update=Time.now.strftime("%m/%d/%Y")
+        else
+            update=Date.strptime(a[0], "%m/%d/%Y")
+        end
+        if a[3] == "" || a[3] == " " || a[3] == nil  
+            contacted=Time.now.strftime("%m/%d/%Y")
+        else
+            contacted=Date.strptime(a[3], "%m/%d/%Y")
+        end
+        @detail=Detail.new({
+        "update_date" => update,
+        "company" => a[1],
+        "contact_name" => a[2],
+        "contact_title" => "",
+        "contact_date" => contacted,
+        "action" => a[4],
+        "method" => a[5],
+        "first_contact" => a[6],
+        "referral" => a[7],
+        "job_title" => a[8],
+        "job_link" => a[9],
+        "notes" => a[10],
+        "step" => a[11],
+        "complete" => a[12],
+        "status" => a[13]
+        })
+        @detail.save
+    end
 end
+# TimeStamp, Company, Contact Name and Title, Date, Action, Method Contacted, First time, Source/referral, Job Title, Link, Notes, Next Step, Complete, Status
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     resources :careers
     resources :details
-    get '/API/import', to: "careers#import"
+    get '/API/import', to: "details#import"
   end
 end
